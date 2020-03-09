8e71f527f2f464d1d81d38770a6ceb5336799b58
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-09 17:11:20 -0400

    Structure the  backend. Create a server using Express

---
+++ backend/data.js
@@ -0,0 +1,45 @@
+export default {
+    products:[{
+        _id:'1',
+        name: 'Slim Shirt',
+        category:'Shirt',
+        image:'/images/d1.jpg',
+        price:23,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:10
+    },
+    {
+        _id:'2',
+        name: 'Corduroy Pants',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:48,
+        brand:'Levis',
+        rating:3.4,
+        numReviews:10
+    },
+    {
+        _id:'3',
+        name: 'Fit Shirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:72,
+        brand:'Adidas',
+        rating:4.8,
+        numReviews:10
+    },
+
+    {
+        _id:'4',
+        name: 'Fit Shirt',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:72,
+        brand:'Adidas',
+        rating:4.8,
+        numReviews:10
+    },
+
+    ]
+}
