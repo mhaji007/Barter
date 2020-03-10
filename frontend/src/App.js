cb435f8bf2c28b60460e1659f7460b2e09b53589
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-10 15:32:27 -0400

    Use hooks to fetch server data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import data from './data';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
